d44b57f12d79b2363ad42cb35f16f97b2077736e
Tyrest <terrifictyler8@gmail.com>
2022-11-10 17:32:33 -0500

    file list stuff

--- renderer.js
+++ renderer.js
@@ -8,19 +8,28 @@ const func = async () => {
 
 const files_to_compile = []
 const file_list = document.getElementById('file-list')
+const compile_container = document.getElementById('compile-container')
 
 document.addEventListener('drop', (event) => {
 	event.preventDefault();
 	event.stopPropagation();
 
     for (const f of event.dataTransfer.files) {
+        file_name = f.path.replace(/^.*[\\\/]/, '')
         // Using the path attribute to get absolute file path
-        console.log('File Path of dragged files: ', f.path)
-        files_to_compile.push(f.path)
-        file_list.appendChild(document.createElement('li')).innerText = f.path
+        console.log('File name of dragged file: ', file_name)
+        files_to_compile.push(file_name)
+        file_list.appendChild(document.createElement('li')).innerText = file_name
     }
 
     console.log(files_to_compile)
+    
+    // filter out only the cpp files
+    cpp_files = files_to_compile.filter((file) => {
+        return file.endsWith('.cpp')
+    })
+
+    compile_container.textContent = "g++ " + cpp_files.join(' ') + " -o output.out"
 });
 
 document.addEventListener('dragover', (e) => {
@@ -35,3 +44,13 @@ document.addEventListener('dragenter', (event) => {
 document.addEventListener('dragleave', (event) => {
 	console.log('File has left the Drop Space');
 });
+
+const clear_files_button = document.getElementById('clear-files-button')
+const compile_button = document.getElementById('compile-button')
+const run_button = document.getElementById('run-button')
+
+clear_files_button.addEventListener('click', () => {
+    files_to_compile.length = 0
+    file_list.innerHTML = ""
+    compile_container.textContent = "No .cpp files detected!"
+})
